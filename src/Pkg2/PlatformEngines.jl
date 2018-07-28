4b08f159265ed14be809e69f647ae2b77f2187e1
sunoru <s@sunoru.com>
2018-07-28 23:44:17 +0800

    `JULIA_HOME` instead of `Sys.BINDIR`.

--- src/Pkg2/PlatformEngines.jl
+++ src/Pkg2/PlatformEngines.jl
@@ -202,7 +202,7 @@ function probe_platform_engines!(;verbose::Bool = false)
         prepend!(compression_engines, [(`7z --help`, gen_7z("7z")...)])
 
         # On windows, we bundle 7z with Julia, so try invoking that directly
-        exe7z = joinpath(Sys.BINDIR, "7z.exe")
+        exe7z = joinpath(JULIA_HOME, "7z.exe")
         prepend!(compression_engines, [(`$exe7z --help`, gen_7z(exe7z)...)])
     end
 
